dccecc4eac6aaa7a837e04754fee68268047bf92
Pratik Thanki <pratikthanki1@gmail.com>
2020-07-03 20:34:01 +0100

    Add method to get the Subboard

--- Revlos/Board.cs
+++ Revlos/Board.cs
@@ -90,18 +90,61 @@ namespace Revlos
             }
             return result;
         }
+
+        public SubBoard GetSubBoard(int x, int y)
+        {
+            if (x < 0 || x > 8 || y < 0 || y > 8)
+                throw new ArgumentOutOfRangeException();
+
+            if (x < 3 && y < 3)
+                return SubBoard.TopLeft;
+
+            if (x > 2 && x < 6 && y < 3)
+                return SubBoard.TopMiddle;
+
+            if (x > 5 && y < 3)
+                return SubBoard.TopRight;
+
+            if (x < 3 && y > 2 && y < 6)
+                return SubBoard.MiddleLeft;
+
+            if (x > 2 && x < 6 && y > 2 && y < 6)
+                return SubBoard.Middle;
+
+            if (x > 5 && y > 2 && y < 6)
+                return SubBoard.MiddleRight;
+
+            if (x < 3 && y > 5)
+                return SubBoard.BottomLeft;
+
+            if (x > 2 && x < 6 && y > 5)
+                return SubBoard.BottomMiddle;
+
+            return SubBoard.BottomRight;
+        }
+
+        public bool IsBoardSolved()
+        {
+            return _board.Cast<BoardSquare>().Any(cell => cell.IsSolved());
+        }
+        
+        public bool IsPossible(BoardSquare square)
+        {
+            
+            return false;
+        }
     }
-    
-    internal enum SubBoards
+
+    public enum SubBoard
     {
-        UpperLeft,
-        UpperMiddle,
-        UpperRight,
+        TopLeft,
+        TopMiddle,
+        TopRight,
         MiddleLeft,
         Middle,
         MiddleRight,
-        LowerLeft,
-        LowerMiddle,
-        LowerRight
+        BottomLeft,
+        BottomMiddle,
+        BottomRight
     }
 }
