711d71e5fe20c3e492a27f3f7bccf6acb8d6298a
Pratik Thanki <pratikthanki1@gmail.com>
2020-07-08 23:20:38 +0100

    Test with new board

--- Revlos/Board.cs
+++ Revlos/Board.cs
@@ -54,7 +54,7 @@ namespace Revlos
                     {
                         Console.Write(" |");
                     }
-                    Console.Write(" " + _board[i, j].ToString());
+                    Console.Write(" " + _board[i, j]);
                 }
 
                 Console.WriteLine();
