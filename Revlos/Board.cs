8c02edf097e4a4611cf4a325fec41b67c06b5f86
Pratik Thanki <pratikthanki1@gmail.com>
2020-08-09 20:06:03 +0100

    Update types and add assertions. Consistent (mostly) one-line returns

--- Revlos/Board.cs
+++ Revlos/Board.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 
 namespace Revlos
@@ -7,24 +8,23 @@ namespace Revlos
     public class Board
     {
         private readonly BoardSquare[,] _board;
-        private HashSet<BoardSquare> _solved;
-        private HashSet<BoardSquare> _unsolved;
-        private Stack<HashSet<BoardSquare>> _changed;
 
         public Board(IReadOnlyList<string> rows)
         {
             _board = BuildBoard(rows);
-            _solved = new HashSet<BoardSquare>();
-            _unsolved = new HashSet<BoardSquare>();
-            _changed = new Stack<HashSet<BoardSquare>>();
+            PrintBoard();
         }
 
         private static BoardSquare[,] BuildBoard(IReadOnlyList<string> rows)
         {
+            Debug.Assert(rows.Count == 9);
+
             var board = new BoardSquare[9, 9];
             for (var i = 0; i < rows.Count; i++)
             {
                 var row = rows[i];
+                Debug.Assert(row.Length == 9);
+
                 for (var j = 0; j < row.Length; j++)
                 {
                     board[i, j] = row[j] == '-'
@@ -46,35 +46,44 @@ namespace Revlos
         {
             for (var i = 0; i < _board.GetLength(0); i++)
             {
-                if (i % 3 == 0 && i > 0)
-                {
-                    Console.WriteLine(" - - - + - - - + - - - ");
-                }
+                if (i % 3 == 0 && i > 0) Console.WriteLine(" - - - + - - - + - - - ");
 
                 for (var j = 0; j < _board.GetLength(1); j++)
                 {
-                    if (j % 3 == 0 && j > 0)
-                    {
-                        Console.Write(" |");
-                    }
+                    if (j % 3 == 0 && j > 0) Console.Write(" |");
 
                     Console.Write(" " + _board[i, j]);
                 }
 
                 Console.WriteLine();
             }
+
+            Console.WriteLine();
         }
 
-        public List<BoardSquare> GetRow(int row)
+        public IEnumerable<BoardSquare> GetRow(int row)
         {
             var result = new List<BoardSquare>(9);
             for (var i = 0; i < _board.GetLength(0); i++)
             {
-                if (i != row)
-                    continue;
+                if (i != row) continue;
 
+                for (var j = 0; j < _board.GetLength(1); j++) result.Add(_board[i, j]);
+
+            }
+
+            return result;
+        }
+
+        public IEnumerable<BoardSquare> GetColumn(int column)
+        {
+            var result = new List<BoardSquare>(9);
+            for (var i = 0; i < _board.GetLength(0); i++)
+            {
                 for (var j = 0; j < _board.GetLength(1); j++)
                 {
+                    if (j != column) continue;
+
                     result.Add(_board[i, j]);
                 }
             }
@@ -82,41 +91,26 @@ namespace Revlos
             return result;
         }
 
-        public List<BoardSquare> GetColumn(int column)
-        {
-            var result = new List<BoardSquare>(9);
-            for (var i = 0; i < _board.GetLength(0); i++)
-            {
-                for (var j = 0; j < _board.GetLength(1); j++)
-                {
-                    if (j != column)
-                        continue;
-
-                    result.Add(_board[i, j]);
-                }
-            }
-
-            return result;
-        }
-
-        public List<BoardSquare> GetSubBoard(SubBoard subBoard)
+        public IEnumerable<BoardSquare> GetSubBoard(SubBoard subBoard)
         {
             var subBoardSquares = new List<BoardSquare>(9);
             for (var i = 0; i < _board.GetLength(0); i++)
             {
                 for (var j = 0; j < _board.GetLength(1); j++)
                 {
-                    if (_board[i, j].GetSubBoard() == subBoard)
-                    {
-                        subBoardSquares.Add(_board[i, j]);
-                    }
+                    if (_board[i, j].GetSubBoard() == subBoard) subBoardSquares.Add(_board[i, j]);
                 }
             }
 
             return subBoardSquares;
         }
 
-        public int SquaresRemaining()
+        public bool IsSolved()
+        {
+            return SquaresRemaining() == 0;
+        }
+
+        private int SquaresRemaining()
         {
             return _board.Cast<BoardSquare>().Count(square => square.IsEmpty());
         }
