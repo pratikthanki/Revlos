07830a7a064e4db2405e595719012c471a710863
Pratik Thanki <pratikthanki1@gmail.com>
2020-08-13 23:58:35 +0100

    Add new puzzle. Move enum to boardsquare.

--- Revlos/Program.cs
+++ Revlos/Program.cs
@@ -11,9 +11,7 @@ namespace Revlos
             foreach (var puzzle in TestPuzzles.puzzles.Take(1))
             {
                 new Backtrack(new Board(puzzle)).Solve();
-
-                // in progress!
-                new DancingLinks(new Board(puzzle)).Solve();
+                // new DancingLinks(new Board(puzzle)).Solve();
             }
         }
     }
