90bd1796d292299918d78e13b671bc9ba3d6130b
Pratik Thanki <pratikthanki1@gmail.com>
2020-08-29 23:06:37 +0100

    refactor methods and properties

--- Revlos/Board.cs
+++ Revlos/Board.cs
@@ -8,10 +8,6 @@ namespace Revlos
     public class Board
     {
         private readonly BoardSquare[,] _board;
-        public Candidate[,] cellConstraint;
-        public Candidate[] rowConstraint;
-        public Candidate[] columnConstraint;
-        public Candidate[,] subBoardConstraint;
 
         public Board(IReadOnlyList<string> rows)
         {
@@ -47,61 +43,6 @@ namespace Revlos
 
         public int GetSize() => _board.GetLength(0);
 
-        public IEnumerable<BoardSquare> GetRow(int rowIndex)
-        {
-            var result = new List<BoardSquare>(9);
-            for (var row = 0; row < _board.GetLength(0); row++)
-            {
-                if (row != rowIndex)
-                    continue;
-
-                for (var col = 0; col < _board.GetLength(1); col++)
-                {
-                    if (!_board[row, col].IsEmpty())
-                        result.Add(_board[row, col]);
-                }
-
-            }
-
-            return result;
-        }
-
-        public IEnumerable<BoardSquare> GetColumn(int colIndex)
-        {
-            var result = new List<BoardSquare>(9);
-            for (var row = 0; row < _board.GetLength(0); row++)
-            {
-                for (var col = 0; col < _board.GetLength(1); col++)
-                {
-                    if (col != colIndex)
-                        continue;
-
-                    if (!_board[row, col].IsEmpty())
-                        result.Add(_board[row, col]);
-                }
-            }
-
-            return result;
-        }
-
-        public IEnumerable<BoardSquare> GetSubBoard(SubBoard subBoard)
-        {
-            var result = new List<BoardSquare>(9);
-            for (var row = 0; row < _board.GetLength(0); row++)
-            {
-                for (var col = 0; col < _board.GetLength(1); col++)
-                {
-                    if (_board[row, col].GetSubBoard() != subBoard)
-                        continue;
-
-                    if (!_board[row, col].IsEmpty())
-                        result.Add(_board[row, col]);
-                }
-            }
-
-            return result;
-        }
-
         public void PrintBoard()
         {
             for (var i = 0; i < _board.GetLength(0); i++)
@@ -121,38 +62,6 @@ namespace Revlos
             }
 
             Console.WriteLine();
-        }
-        
-        private void InitialiseConstraints()
-        {
-            for (var row = 0; row < 9; row++)
-            for (var col = 0; col < 9; col++)
-                cellConstraint[row, col] = new Candidate(9, true);
-
-            for (var row = 0; row < 3; row++)
-            for (var col = 0; col < 3; col++)
-                subBoardConstraint[row, col] = new Candidate(9, false);
-
-
-            for (var i = 0; i < 9; i++)
-            {
-                rowConstraint[i] = new Candidate(9, false);
-                columnConstraint[i] = new Candidate(9, false);
-            }
-
-            for (var row = 0; row < 9; row++)
-            {
-                for (var col = 0; col < 9; col++)
-                {
-                    if (_board[row, col].GetValue() <= 0)
-                        continue;
-
-                    var candidate = _board[row, col].GetValue();
-                    rowConstraint[row][candidate] = true;
-                    columnConstraint[col][candidate] = true;
-                    subBoardConstraint[row / 3, col / 3][candidate] = true;
-                }
-            }
         }
     }
 }
--- Revlos/BoardSquare.cs
+++ Revlos/BoardSquare.cs
@@ -1,11 +1,9 @@
-using System;
 
 namespace Revlos
 {
     public class BoardSquare
     {
         private int _value;
-        private SubBoard _subBoard;
         private int row;
         private int col;
 
@@ -20,62 +18,18 @@ namespace Revlos
 
         public int GetColumnIndex() => col;
 
-        public SubBoard GetSubBoard() => _subBoard;
-
         public void SetValue(int value) => _value = value;
 
         public void SetLocation(int rowIndex, int colIndex)
         {
             row = rowIndex;
             col = colIndex;
-            _subBoard = GetSubBoard(colIndex, rowIndex);
         }
 
         public bool IsEmpty() => _value == 0;
 
-        private static SubBoard GetSubBoard(int x, int y)
-        {
-            if (x < 0 || x > 8 || y < 0 || y > 8) throw new ArgumentOutOfRangeException();
-            if (x < 3 && y < 3) return SubBoard.TopLeft;
-            if (x > 2 && x < 6 && y < 3) return SubBoard.TopMiddle;
-            if (x > 5 && y < 3) return SubBoard.TopRight;
-            if (x < 3 && y > 2 && y < 6) return SubBoard.MiddleLeft;
-            if (x > 2 && x < 6 && y > 2 && y < 6) return SubBoard.Middle;
-            if (x > 5 && y > 2 && y < 6) return SubBoard.MiddleRight;
-            if (x < 3 && y > 5) return SubBoard.BottomLeft;
-            if (x > 2 && x < 6 && y > 5) return SubBoard.BottomMiddle;
-
-            return SubBoard.BottomRight;
-        }
-        
         public BoardSquare Clone() => new BoardSquare(this._value);
 
         public override string ToString() => _value == 0 ? " " : _value.ToString();
-    }
-
-    public enum SubBoard
-    {
-        TopLeft,
-        TopMiddle,
-        TopRight,
-        MiddleLeft,
-        Middle,
-        MiddleRight,
-        BottomLeft,
-        BottomMiddle,
-        BottomRight
-    }
-
-    public enum SubBoard
-    {
-        TopLeft,
-        TopMiddle,
-        TopRight,
-        MiddleLeft,
-        Middle,
-        MiddleRight,
-        BottomLeft,
-        BottomMiddle,
-        BottomRight
     }
 }
