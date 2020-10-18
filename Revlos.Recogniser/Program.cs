021bdc1da82df862960413be64118498b4a8c4a2
Pratik Thanki <pratikthanki1@gmail.com>
2020-10-18 12:53:36 +0100

    New ml project for puzzle recogniser

---
+++ Revlos.Recogniser/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.ML;
+
+namespace Revlos.Recogniser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var mlContext = new MLContext();
+        }
+    }
+}
